ebb48d7318a03f8da617484aff92b8504de697ee
janbar <jlbarriere68@gmail.com>
2023-03-18 01:26:24 +0100

    revert drop stid135

--- drivers/src/stv091x.c
+++ drivers/src/stv091x.c
@@ -1012,7 +1012,9 @@ static int Start(struct stv *state, struct dtv_frontend_properties *p)
 	if (p->scrambling_sequence_index)
 		SetPLS(state, 1, p->scrambling_sequence_index);
 
-//	SetModcode(state, p->modcode);
+#ifdef DTV_MODCODE
+	SetModcode(state, p->modcode);
+#endif
 
 	/* Set the Init Symbol rate*/
 	symb = MulDiv32(p->symbol_rate, 65536, state->base->mclk);
